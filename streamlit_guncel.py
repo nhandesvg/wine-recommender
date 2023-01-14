5a8150bb09edf009632e33ae3641adfca1859e03
nhandesvg <121826158+nhandesvg@users.noreply.github.com>
2023-01-14 22:02:19 +0300

    Add files via upload

--- streamlit_guncel.py
+++ streamlit_guncel.py
@@ -20,7 +20,7 @@ st.set_page_config(page_title="Wine Recommender", page_icon="🍷", layout="cent
 # st.image(image, width=150)
 
 #df = pd.read_csv("/Users/handesevgi/PycharmProjects/pythonProject2/Proje/preprocessed_wine_df.csv")
-df=pd.read_csv("/Users/handesevgi/hande-github/wine-recommender/preprocessed_wine_df.csv.zip", compression='zip')
+df=pd.read_csv("nhandesvg/wine-recommender/preprocessed_wine_df.csv.zip", compression='zip')
 df.head()
 
 list_country = df["country"].unique()
